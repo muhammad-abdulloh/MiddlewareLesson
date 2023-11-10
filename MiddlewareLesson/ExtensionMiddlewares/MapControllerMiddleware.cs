eb40076435e74b63051c13ac4581003afe2dfa4c
muhammad-abdulloh <clevercoderr@gmail.com>
2023-11-10 22:03:42 +0500

    Add project files.

---
+++ MiddlewareLesson/ExtensionMiddlewares/MapControllerMiddleware.cs
@@ -0,0 +1,14 @@
+﻿using MiddlewareLesson.Middlewares;
+
+namespace MiddlewareLesson.ExtensionMiddlewares
+{
+    public static class MapControllerMiddleware
+    {
+        public static IApplicationBuilder ConfigureRedirectionMap(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<RequestMiddleware>();
+
+            return app;
+        }
+    }
+}
